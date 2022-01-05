0fa9d93cc8111313979ae2131e21f7347b41de54
Augustin Norbert Justin Kapps <kapps@izar.ib0.cm.izar.local>
2022-01-05 12:33:20 +0100

    added linear model

--- data_processing/prepare_raw_features.py
+++ data_processing/prepare_raw_features.py
@@ -4,52 +4,66 @@ from config.get import cfg
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from TokenStandardScaler import TokenStandardScaler
+from sklearn.model_selection import train_test_split
+
 print("===================================")     
-data = pd.read_csv(cfg['files']['preprocessed_data'],nrows=25_000_000)
+data = pd.read_csv(cfg['files']['preprocessed_data'],nrows=5_000_000).drop(columns=["time"])
+data = data.set_index(["cycle_id","token1","token2"])
 
-# from data exploration log was found to be a good normaliser
-def log_scaling(data):
-    data['baseAmount']  = np.log(data.baseAmount)
-    data['quoteAmount'] = np.log(data.quoteAmount)
-    data['quotePrice']  = np.log(data.quotePrice)
-    return data;
 
-log_data = log_scaling(data).dropna()
-print(len(log_data)," rows loaded")
-N_TOKEN = 3 # cycle length
-K = 2       # quote price & gasPrice
-N =data.cycle_id.nunique() # number of cycles
-P = 600     # max time series length per cycle
+# train test split
+print("splitting")
+train_ix, test_ix = train_test_split(data.index.levels[0],train_size=0.8)
+X_train = data.loc[train_ix]
+X_test = data.loc[test_ix]
+print(f"Shapes : X_train={X_train.shape}, X_test={X_test.shape}")
+# personal rescaling
+scaler   = TokenStandardScaler()
+tX_train = scaler.fit_transform(X_train)
+tX_test  = scaler.transform(X_test)
 
-def build_tensor(data):
-    tensor = np.zeros((N, N_TOKEN,P, K))
-    cycle_ids = np.zeros(N)
+def pad(X):
+    def pad_index(index,P=600):
+        padded_index = pd.MultiIndex.from_product([list(index.unique()),range(P)])
+        as_frame = padded_index.to_frame()
+        as_frame['cycle_id'], as_frame['token1'], as_frame['token2'] = zip(*as_frame[0])
+        return as_frame.reset_index().drop(columns=["level_0","level_1",0]).set_index(["cycle_id","token1","token2",1]).index
+    
+    padded_index = pad_index(X.index,600)
+    X[1] = X.reset_index().groupby(["cycle_id","token1","token2"]).cumcount().values
+    return X.reset_index().set_index(["cycle_id","token1","token2",1]).reindex(padded_index,fill_value=0)
+
+
+def build_tensor(X_padded):
+    cycle_ids = []
+    tensor    = []
     i = 0
-    def get_sorted_token_map(g):
-        t = g[['token1','token2']].values
-        u, ind = np.unique(t, return_index=True)
-        u_sorted =  u[np.argsort(ind)]
-        return dict(zip(u_sorted, range(len(u_sorted))))
+    for cycle_id, group in iter(X_padded.groupby("cycle_id")): 
+            try :
+                tensor.append(group[["quotePrice","gasPrice"]].values.reshape((3, 600, 2)))
+                cycle_ids.append(cycle_id)
+            except:
+                print(i)
+            i+=1
+            
+    return np.array(cycle_ids),np.array(tensor) 
 
-    for cycle_id, group in iter(data.groupby('cycle_id')):
-        token_map = get_sorted_token_map(group)
-        cycle_ids[i] =cycle_id
-        for _, g in iter(group.groupby(['token1','token2'])):
-            a = g[['quotePrice','gasPrice']].values 
-            # zero padding
-            padded = np.pad(a, [(0, P - len(a)),(0,0)])
-            # assign and reshape into a matrix
-            first_token = g.token1.iloc[0]
-            token_ind = token_map[first_token]
-            tensor[i,token_ind,:,:] = padded.reshape(1,P,K)
-        i = i+1
-    return cycle_ids[:-1],tensor[:-1]
-   
-print("Processing")
-cycle_ids,X = build_tensor(log_data)
-print("data shape : ",X.shape)
-print("ids shape : ",cycle_ids.shape)
+print("padding")
+train_padded = pad(tX_train)
+test_padded = pad(tX_test)
+print(f"Shapes : train_padded={train_padded.shape}, test_padded={test_padded.shape}")
+
+print("building tensor")
+train_ids , train_tensor = build_tensor(train_padded)
+test_ids  , test_tensor  = build_tensor(test_padded)
+print(f"Shapes : train_tensor={train_tensor.shape}, test_tensor={test_tensor.shape}")
+
+
+
 print("Saving")
-np.save(cfg['files']['raw_features'],X)
-np.save(cfg['files']['cycle_ids'],cycle_ids)
+np.save(cfg['files']['raw_test_features'] , test_tensor)
+np.save(cfg['files']['raw_train_features'] ,train_tensor)
+np.save(cfg['files']['test_ids'] , test_ids)
+np.save(cfg['files']['train_ids'] , train_ids)
 print("Done")
