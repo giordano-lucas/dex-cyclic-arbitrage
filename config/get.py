0e9dcf03d23d4008b2a272d1f726d972eaa77881
Lucas Giordano <giordano3102lucas@gmail.com>
2022-01-06 10:59:51 +0100

    add_base_dir to config

--- config/get.py
+++ config/get.py
@@ -3,6 +3,22 @@ import yaml
 import sys,os
 
 base_dir = '/'.join(os.getcwd().split('/')[:4])
+
+def add_base_dir(value, acc, key):
+    if not isinstance(value, dict):
+        acc[key] = f"{base_dir}/{value}"
+    else:
+        if key is None:
+            param = acc
+        else:
+            acc[key] = {};
+            param = acc[key]
+        for k,v in value.items():
+            add_base_dir(v, param, k)    
+    return acc
+
 with open(f"{base_dir}/config/config.yml", "r") as ymlfile:
-    cfg = yaml.safe_load(ymlfile)
+    raw_cfg = yaml.safe_load(ymlfile)
+    cfg = add_base_dir(raw_cfg, {}, None)
+
 
