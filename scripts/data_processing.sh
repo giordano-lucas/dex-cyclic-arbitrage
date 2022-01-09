482ad099d68939d400c3784347207df4d47e0124
Lucas Giordano <giordano3102lucas@gmail.com>
2022-01-09 11:08:14 +0100

    update names in build_prediction_data + add compte embedding in data_processing pipeline

--- scripts/data_processing.sh
+++ scripts/data_processing.sh
@@ -6,5 +6,7 @@ python3 filter_cycles.py
 python3 combine_raw_data.py
 # build tensor for embedding
 python3 build_embedding_features.py
+# compute the embeddings
+python3 compute_embeddings.py
 # build prediction data
 python3 build_prediction_data.py
