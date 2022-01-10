30dda4ece29835f087edd67bef4807ebfc80a242
Lucas Giordano <giordano3102lucas@gmail.com>
2022-01-10 22:10:47 +0100

    fix merge conflics

--- data_processing/build_embedding_features.py
+++ data_processing/build_embedding_features.py
@@ -8,22 +8,8 @@ from sklearn.preprocessing import StandardScaler
 from TokenStandardScaler import TokenStandardScaler
 from sklearn.model_selection import train_test_split
 
-
-def pad(X):
-    def pad_index(index,P=600):
-        padded_index = pd.MultiIndex.from_product([list(index.unique()),range(P)])
-        as_frame = padded_index.to_frame()
-        as_frame['cycle_id'], as_frame['token1'], as_frame['token2'] = zip(*as_frame[0])
-        return as_frame.reset_index().drop(columns=["level_0","level_1",0]).set_index(["cycle_id","token1","token2",1]).index
-    
-    padded_index = pad_index(X.index,600)
-    X[1] = X.reset_index().groupby(["cycle_id","token1","token2"]).cumcount().values
-    return X.reset_index().set_index(["cycle_id","token1","token2",1]).reindex(padded_index,fill_value=0).dropna()
-
-<<<<<<< HEAD
 def build_tensor(data):
     data = data.reset_index()
-    
     
     N_TOKEN = 3 # cycle length
     K = 2       # quote price & gasPrice
@@ -55,13 +41,14 @@ def build_tensor(data):
 
 
 def run(use_liquid = True ,nrows=10_000_000):  
+    # when files are loaded or store => add _liquid at the end of the name
+    features_dir = 'liquid' if use_liquid else 'full'
     cols = ["quotePrice","gasPrice"]
-    
     print("loading data")
     if use_liquid:
-        data = pd.read_csv(cfg['files']['liquid_preprocessed_data'],nrows=nrows)
+        data = pd.read_csv(cfg['files'][features_dir]['liquid_preprocessed_data'],nrows=nrows)
     else :
-        data = pd.read_csv(cfg['files']['preprocessed_data'],nrows=nrows)
+        data = pd.read_csv(cfg['files'][features_dir]['preprocessed_data'],nrows=nrows)
         
     data = data.drop(columns=["time"]).set_index(["cycle_id","token1","token2"])[cols]
     
@@ -69,27 +56,6 @@ def run(use_liquid = True ,nrows=10_000_000):
 #     print(f"taking the log of {cols}")
 #     data = np.log(data).dropna()
     
-=======
-
-def build_tensor(X_padded):
-    cycle_ids = []
-    tensor    = []
-    errors = 0
-    for cycle_id, group in iter(X_padded.groupby("cycle_id")): 
-            try :
-                tensor.append(group[["quotePrice","gasPrice"]].values.reshape((3, 600, 2)))
-                cycle_ids.append(cycle_id)
-            except:
-                errors+=1
-    print(f"{errors} errors")   
-    return np.array(cycle_ids),np.array(tensor) 
-
-def run():   
-    data = pd.read_csv(cfg['files']['preprocessed_data'],nrows=10_000_000).drop(columns=["time"]) 
-    data = data.set_index(["cycle_id","token1","token2"])
-
-
->>>>>>> 2b113825b93622f4f7acce54610bc578cff984a4
     # train test split
     print("splitting")
     train_ix, test_ix = train_test_split(data.index.levels[0],train_size=0.8)
@@ -102,13 +68,11 @@ def run():
     tX_test  = scaler.transform(X_test)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 2b113825b93622f4f7acce54610bc578cff984a4
-    print("padding")
-    train_padded = pad(tX_train)
-    test_padded = pad(tX_test)
+    #print("padding")
+    #train_padded = pad(tX_train)
+    #test_padded = pad(tX_test)
+    train_padded = tX_train
+    test_padded  = tX_test
     print(f"Shapes : train_padded={train_padded.shape}, test_padded={test_padded.shape}")
 
     print("building tensor")
@@ -116,31 +80,19 @@ def run():
     test_ids  , test_tensor  = build_tensor(test_padded)
     print(f"Shapes : train_tensor={train_tensor.shape}, test_tensor={test_tensor.shape}")
 
-
-
     print("Saving")
-<<<<<<< HEAD
     if use_liquid:
-        np.save(cfg['files']['raw_test_features_liquid'] , test_tensor)
-        np.save(cfg['files']['raw_train_features_liquid'] ,train_tensor)
-        np.save(cfg['files']['test_ids_liquid'] , test_ids)
-        np.save(cfg['files']['train_ids_liquid'] , train_ids)
+        np.save(cfg['files'][features_dir]['raw_test_features_liquid'] , test_tensor)
+        np.save(cfg['files'][features_dir]['raw_train_features_liquid'] ,train_tensor)
+        np.save(cfg['files'][features_dir]['test_ids_liquid'] , test_ids)
+        np.save(cfg['files'][features_dir]['train_ids_liquid'] , train_ids)
     else : 
-        np.save(cfg['files']['raw_test_features'] , test_tensor)
-        np.save(cfg['files']['raw_train_features'] ,train_tensor)       
-        np.save(cfg['files']['test_ids'] , test_ids)
-        np.save(cfg['files']['train_ids'] , train_ids)
+        np.save(cfg['files'][features_dir]['raw_test_features'] , test_tensor)
+        np.save(cfg['files'][features_dir]['raw_train_features'] ,train_tensor)       
+        np.save(cfg['files'][features_dir]['test_ids'] , test_ids)
+        np.save(cfg['files'][features_dir]['train_ids'] , train_ids)
 
     
-=======
- 
-    np.save(cfg['files']['raw_test_features'] , test_tensor)
-    np.save(cfg['files']['raw_train_features'] ,train_tensor)
-    np.save(cfg['files']['test_ids'] , test_ids)
-    np.save(cfg['files']['train_ids'] , train_ids)
-    print("Done")
-
->>>>>>> 2b113825b93622f4f7acce54610bc578cff984a4
 if __name__ == "__main__":
     print("==== Run : build embedding features ====")
     check_and_create_dir(cfg['directories']['ML_features'])
