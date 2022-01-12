818389ffdbdee7727ecafdd204f64c0c1619f9ca
Lucas Giordano <giordano3102lucas@gmail.com>
2022-01-12 11:28:20 +0100

    readme and comments on rule based features

--- models/prediction/build_rule_based_features/build_rule_based_scaled_encoded_features.py
+++ models/prediction/build_rule_based_features/build_rule_based_scaled_encoded_features.py
@@ -2,14 +2,20 @@ import sys, os
 sys.path.append('/'.join(os.getcwd().split('/')[:4]))
 
 import data_processing.build_embedding_features as bef
-import models.prediction.build_ruled_based_features.build_ruled_based_encoded_features as brbef
+import models.prediction.build_rule_based_features.build_rule_based_encoded_features.py as brbef
+
 base_config = {
         'use_liquid': True , 
         'nrows':10_000_000, 
         'drop_columns':None,
         'log_transformation':False,
         'new_train_idx':False,
-        'extra_dir': 'ruled_based'
+        'extra_dir': 'ruled_based',
+        'skip_split': True,
+        'feature_name': 'encoded',
+        'scaling':False,
 }
+# construct encoded train/test sets
 brbef.run()
-bef.run(skip_split=True, feature_name='encoded', **base_config, scaling=False)
+# scaling and tensor building
+bef.run(**base_config)
